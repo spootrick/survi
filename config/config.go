c16b6585550510dbc2469374c4665036bbcc2d11
Furkan Karakoyunlu <furkan.karakoyunlu@sap.com>
2020-04-20 21:54:48 +0300

    disable ssl mode on db connection

--- config/config.go
+++ config/config.go
@@ -39,6 +39,6 @@ func loadPortFromEnv() {
 }
 func loadDBDetailsFromEnv() {
 	DBDriver = os.Getenv("DB_DRIVER")
-	DBUrl = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", os.Getenv("DB_HOST"),
+	DBUrl = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASS"))
 }
