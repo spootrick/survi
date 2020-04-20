7318665c90af3dedc4759e007b4e36dc53b38932
Furkan Karakoyunlu <furkan.karakoyunlu@sap.com>
2020-04-20 21:55:46 +0300

    add comment about ssl mode

--- config/config.go
+++ config/config.go
@@ -41,4 +41,5 @@ func loadDBDetailsFromEnv() {
 	DBDriver = os.Getenv("DB_DRIVER")
 	DBUrl = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASS"))
+	// TODO: Do not forget to enable SSL mode
 }
