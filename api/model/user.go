aeaa8effc1ee2a94196dc6330cbd9677f0c9fcd0
Furkan Karakoyunlu <furkan.karakoyunlu@sap.com>
2020-04-23 22:12:10 +0300

    fix user model

--- api/model/user.go
+++ api/model/user.go
@@ -9,8 +9,8 @@ type User struct {
 	ID        uint      `gorm:"PRIMARY_KEY;AUTO_INCREMENT" json:"id"`
 	FirstName string    `gorm:"size:50;NOT NULL" json:"first_name"`
 	LastName  string    `gorm:"size:50;NOT NULL" json:"last_name"`
-	Email     string    `gorm:"size:50;NOT NULL;unique_index" json:"email"`
-	Password  string    `gorm:"size:100;NOT NULL" json:"-"`
+	Email     string    `gorm:"size:50;NOT NULL;unique;unique_index" json:"email"`
+	Password  string    `gorm:"size:100;NOT NULL" json:"password"`
 	Roles     string    `gorm:"size:50;NOT NULL;default:'ROLE_USER'" json:"roles"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
