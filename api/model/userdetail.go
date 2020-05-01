c2979c74e32a32cbcd4aa247c4ea5090f150b965
Furkan Karakoyunlu <furkan.karakoyunlu@sap.com>
2020-05-01 13:00:03 +0300

    make user id unique in user details

--- api/model/userdetail.go
+++ api/model/userdetail.go
@@ -9,7 +9,7 @@ import (
 type UserDetail struct {
 	ID         uint      `gorm:"PRIMARY_KEY;AUTO_INCREMENT" json:"id"`
 	User       User      `json:"user"` // gorm finds foreign key as UserID
-	UserID     uint      `gorm:"NOT NULL" json:"user_id"`
+	UserID     uint      `gorm:"unique;NOT NULL" json:"user_id"`
 	BirthDate  time.Time `gorm:"type:DATE NOT NULL" json:"birth_date"`
 	Gender     string    `gorm:"size:6;NOT NULL" json:"gender"`
 	Profession string    `gorm:"size:100;" json:"profession;omitempty"`
